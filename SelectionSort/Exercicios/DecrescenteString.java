d0996f4c995df2fb7b58a5c534d805ccb3b2c5e0
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-08-25 10:48:10 -0300

    Create DecrescenteString.java

---
+++ SelectionSort/Exercicios/DecrescenteString.java
@@ -0,0 +1,32 @@
+package SelectionSort.Exercicios;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Collections;
+
+
+public class DecrescenteString {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        final int TAM;
+
+         System.out.println("--- Ordena Vetor String (decrescente) ---");
+         System.out.print("Informe o tamanho do vetor: ");
+         TAM = input.nextInt();
+         String[] nomes = new String[TAM];
+         
+         for(int i = 0; i < nomes.length; i++){
+             System.out.print("Digite uma palavra: ");
+             nomes[i] = input.next();
+         }
+         Arrays.sort(nomes, Collections.reverseOrder());
+         for (String nome : nomes) {
+            System.out.print(nome + " | ");
+        }
+         
+         input.close();
+    }
+    
+}
