70ad19c787c612e8167f40e8077b3565f3563913
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-09-09 14:54:12 -0300

    Create TesteSomaIterativa.java

---
+++ Recursividade/Exercicios/TesteSomaIterativa.java
@@ -0,0 +1,11 @@
+package Recursividade.Exercicios;
+
+public class TesteSomaIterativa {
+
+	public static void main(String[] args) {
+		
+		System.out.println(IterativaSoma.SomaIterativa(2));
+
+	}
+
+}
