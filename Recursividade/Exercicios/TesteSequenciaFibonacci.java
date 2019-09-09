3d3c601423b6ff1027f00415720613acb0ff75c3
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-09-09 14:54:04 -0300

    Create TesteSequenciaFibonacci.java

---
+++ Recursividade/Exercicios/TesteSequenciaFibonacci.java
@@ -0,0 +1,16 @@
+package Recursividade.Exercicios;
+
+public class TesteSequenciaFibonacci {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sequencia Fibonacci");
+		for(int i = 0; i < 10; i++) {
+			System.out.print(" | "+SequenciaFibonacci.Fibonacci(i));
+		}
+		
+		
+
+	}
+
+}
