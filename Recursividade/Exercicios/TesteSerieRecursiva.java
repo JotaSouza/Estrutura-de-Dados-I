202bc7929171a5ab1e068ebee9c952f3986a2901
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-09-09 14:54:10 -0300

    Create TesteSerieRecursiva.java

---
+++ Recursividade/Exercicios/TesteSerieRecursiva.java
@@ -0,0 +1,11 @@
+package Recursividade.Exercicios;
+
+public class TesteSerieRecursiva {
+
+	public static void main(String[] args) {
+		
+		System.out.println(SerieRecursiva.CalculaValor(4));
+
+	}
+
+}
