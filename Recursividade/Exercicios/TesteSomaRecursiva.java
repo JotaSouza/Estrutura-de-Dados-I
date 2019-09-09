015d2cb4464db7fe7761d476e2252c5511b1c4e6
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-09-09 14:54:15 -0300

    Create TesteSomaRecursiva.java

---
+++ Recursividade/Exercicios/TesteSomaRecursiva.java
@@ -0,0 +1,11 @@
+package Recursividade.Exercicios;
+
+public class TesteSomaRecursiva {
+
+	public static void main(String[] args) {
+		
+		System.out.println(SomaRecursividade.SomaRecursiva(4));
+
+	}
+
+}
