0618d67f08d4223464972adeb95e21844b477ba5
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-09-09 14:54:07 -0300

    Create TesteSerie2Recursiva.java

---
+++ Recursividade/Exercicios/TesteSerie2Recursiva.java
@@ -0,0 +1,11 @@
+package Recursividade.Exercicios;
+
+public class TesteSerie2Recursiva {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Serie2Recursiva.CalculaSerie(10));
+
+	}
+
+}
