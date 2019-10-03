3940249621f2563bdfe0b539b56bcab6627e1dc2
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-10-03 16:53:15 -0300

    Create BubbleSort.java

---
+++ Trabalhos/MetodosOrdenacao/BubbleSort.java
@@ -0,0 +1,41 @@
+package MetodosOrdenacao;
+
+public class BubbleSort {
+
+	//método qe faz a ordenação do vetor
+	public int[] ordenar(int[] vetor){
+		final int MAX = vetor.length;
+		for(int i = 0; i < MAX; i++){
+			for(int j = 0; j < MAX - 1; j++){
+				if(vetor[j] > vetor[j+1]){
+					int aux = vetor[j];
+					vetor[j] = vetor[j+1];
+					vetor[j+1] = aux;
+				}
+			}
+		}
+		return vetor;
+	}
+	
+	//método que retorna as comparações de trocas e o tempo
+	public long[] retornaComparacaoTrocaTempo(int[] vetor){
+		long comparar = 0;
+		long trocar = 0;
+		long tempoIni = System.nanoTime();
+		long tempoFim = 0;
+		final int MAX = vetor.length;
+		for(int i = 0; i < MAX; i++){
+			for(int j = 0; j < MAX - 1; j++){
+				comparar++;
+				if(vetor[j] > vetor[j+1]){
+					int aux = vetor[j];
+					vetor[j] = vetor[j+1];
+					vetor[j+1] = aux;
+					trocar++;
+			}
+		}
+	}
+		tempoFim = System.nanoTime();
+		return new long[] {	comparar, trocar, (tempoFim - tempoIni)	};
+  }
+}
