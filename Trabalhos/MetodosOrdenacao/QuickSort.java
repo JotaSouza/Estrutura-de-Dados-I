4c38611a8774f70fc997323628cd52573cee5964
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-10-03 16:53:26 -0300

    Create QuickSort.java

---
+++ Trabalhos/MetodosOrdenacao/QuickSort.java
@@ -0,0 +1,72 @@
+package MetodosOrdenacao;
+
+public class QuickSort {
+
+	private long comparar = 0;
+	private long trocar = 0;
+	private long tempoIni = 0;
+	private long tempoFim = 0;
+	
+	//método que faz a ordenação do vetor, comparação e verificação do tempo
+	public void ordena(int[] vetor, int inicio, int fim){
+		this.tempoIni = System.nanoTime();
+		int meio;
+		if(inicio < fim){
+			meio = particao(vetor, inicio, fim);
+			ordena(vetor, inicio, meio);
+			ordena(vetor, meio + 1, fim);
+		}
+		this.tempoFim = System.nanoTime();
+	}
+	
+	public int particao(int[] vetor, int inicio, int fim){
+		int pivo = vetor[(inicio + fim) / 2];
+		int i = inicio - 1;
+		int j = fim + 1;
+		while(i < j){
+			do{
+				j--;
+				this.comparar++;
+			}while(vetor[j] > pivo);
+			do{
+				i++;
+				this.comparar++;
+			}while(vetor[i] < pivo);
+			
+			if(i < j){
+				int auxiliar = vetor[i];
+				vetor[i] = vetor[j];
+				vetor[j] = auxiliar;
+				this.trocar++;
+			}
+		}
+		return j;
+	}
+	
+	public long getComparar() {
+		return comparar;
+	}
+	public void setComparar(long comparar) {
+		this.comparar = comparar;
+	}
+	public long getTrocar() {
+		return trocar;
+	}
+	public void setTrocar(long trocar) {
+		this.trocar = trocar;
+	}
+	public long getTempoIni() {
+		return tempoIni;
+	}
+	public void setTempoIni(long tempoIni) {
+		this.tempoIni = tempoIni;
+	}
+	public long getTempoFim() {
+		return tempoFim;
+	}
+	public void setTempoFim(long tempoFim) {
+		this.tempoFim = tempoFim;
+	}
+	
+	
+}
