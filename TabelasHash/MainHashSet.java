d6ae0ef948190afd293fe004199557c5f8e7fa8c
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-11-10 20:45:33 -0300

    Create MainHashSet.java

---
+++ TabelasHash/MainHashSet.java
@@ -0,0 +1,18 @@
+package TabelasHash;
+
+import java.util.HashSet;
+
+public class MainHashSet {
+
+	public static void main(String[] args) {
+		HashSet<Object> conjunto = new HashSet<Object>();
+		
+		conjunto.add(1);
+		conjunto.add("Fabio");
+		conjunto.add("Juca");
+		
+		System.out.println(conjunto);
+
+	}
+
+}
