d1638cba51a37d068edb8b21cbde57e409e41114
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-11-10 20:45:29 -0300

    Create MainHash.java

---
+++ TabelasHash/MainHash.java
@@ -0,0 +1,14 @@
+package TabelasHash;
+
+public class MainHash {
+
+	public static void main(String[] args) {
+		OperacoesHash op = new OperacoesHash();
+		
+		op.setChave(005105);
+		op.adiciona(1390);
+		System.out.println(op.adiciona(1390));
+
+	}
+
+}
